98d79afb090f55d42d4b4c09a3ac647c08c3244b
VictorFranciscoFerreira <victorfranciscoferreira@gmail.com>
2022-04-14 16:18:03 -0300

    Requisito 1 feito

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,13 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aquiaa
+function getSpeciesByIds(...ids) {
+  const speciesById = [...ids];
+  return species.filter((especie) => speciesById.includes(especie.id));
 }
 
+// console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
+// console.log(getSpeciesByIds());
+// console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce',
+//   '533bebf3-6bbe-41d8-9cdf-46f7d13b62ae'));
 module.exports = getSpeciesByIds;
