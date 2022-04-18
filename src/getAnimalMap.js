cefda7de5d5ab91b91df99a74548cb3d8bbbdb29
VictorFranciscoFerreira <victorfranciscoferreira@gmail.com>
2022-04-18 17:46:23 -0300

    requisito 9 feito

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,42 @@
 const data = require('../data/zoo_data');
 
+// const expected = {
+//   NE: ['lions', 'giraffes'],
+//   NW: ['tigers', 'bears', 'elephants'],
+//   SE: ['penguins', 'otters'],
+//   SW: ['frogs', 'snakes'],
+// };
+
+const zooLocation = () => {
+  const mapLocation = {
+    NE: data.species.filter((location) => location.location === 'NE')
+      .map((animal) => animal.name),
+    NW: data.species.filter((location) => location.location === 'NW')
+      .map((animal) => animal.name),
+    SE: data.species.filter((location) => location.location === 'SE')
+      .map((animal) => animal.name),
+    SW: data.species.filter((location) => location.location === 'SW')
+      .map((animal) => animal.name),
+};
+  return mapLocation;
+};
+
 function getAnimalMap(options) {
-  // seu código aqui
+  if (!options || includeNames === undefined) {
+    return zooLocation();
+  }
+  if (includeNames === true) {
+    const mapLocation = {
+      NE: data.species.filter((location) => location.location === 'NE')
+        .map((animal) => animal.residents),
+      NW: data.species.filter((location) => location.location === 'NW')
+        .map((animal) => animal.residents),
+      SE: data.species.filter((location) => location.location === 'SE')
+        .map((animal) => animal.residents),
+      SW: data.species.filter((location) => location.location === 'SW')
+        .map((animal) => animal.residents),
+    }
+  }
 }
 
 module.exports = getAnimalMap;
--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,12 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const employee = employees.find((person) => person.id === id);
+  const firstAnimal = employee.responsibleFor[0];
+  const verifyAnimal = data.species.find((animal) => animal.id === firstAnimal);
+  const oldestAnimal = verifyAnimal.residents.sort((a, b) => b.age - a.age);
+  return [oldestAnimal[0].name, oldestAnimal[0].sex, oldestAnimal[0].age];
 }
 
 module.exports = getOldestFromFirstSpecies;
