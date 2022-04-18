899dac9f0036ea5c5a1bf64a1aef8307a4b7be03
VictorFranciscoFerreira <victorfranciscoferreira@gmail.com>
2022-04-18 17:22:42 -0300

    requisito 8 feito

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,62 @@
 const data = require('../data/zoo_data');
 
+const days = Object.keys(data.hours);
+
+const animalsPerDay = (day) => {
+  const dayExhibit = [];
+  data.species.forEach((animal) => {
+    if (animal.availability.includes(day)) {
+      dayExhibit.push(animal.name);
+    }
+  });
+  return dayExhibit;
+};
+
+const withoutParam = () => {
+  const schedule = {};
+  days.forEach((day) => {
+    if (day === 'Monday') {
+      schedule[day] = {
+        officeHour: 'CLOSED', exhibition: 'The zoo will be closed!',
+      };
+      return schedule;
+    }
+    schedule[day] = {
+      officeHour: `Open from ${data.hours[day].open}am until ${data.hours[day].close}pm`,
+      exhibition: animalsPerDay(day),
+    };
+  });
+  return schedule;
+};
+
+const dayParam = (day) => {
+  const schedule = {};
+  if (day === 'Monday') {
+    schedule[day] = {
+      officeHour: 'CLOSED', exhibition: 'The zoo will be closed!',
+    };
+    return schedule;
+  }
+  schedule[day] = {
+    officeHour: `Open from ${data.hours[day].open}am until ${data.hours[day].close}pm`,
+    exhibition: animalsPerDay(day),
+  };
+  return schedule;
+};
+
+const animalParam = (animal) => {
+  const exhibit = data.species.find((ani) => ani.name === animal).availability;
+  return exhibit;
+};
+
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  if (data.species.find((animal) => animal.name === scheduleTarget)) {
+    return animalParam(scheduleTarget);
+  }
+  if (days.includes(scheduleTarget)) {
+    return dayParam(scheduleTarget);
+  }
+  return withoutParam();
 }
 
 module.exports = getSchedule;
