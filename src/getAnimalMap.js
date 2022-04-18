47cb7e644666d597f37df17bc1d6b50258086f19
VictorFranciscoFerreira <victorfranciscoferreira@gmail.com>
2022-04-18 17:50:18 -0300

    lint corrections

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -7,36 +7,36 @@ const data = require('../data/zoo_data');
 //   SW: ['frogs', 'snakes'],
 // };
 
-const zooLocation = () => {
-  const mapLocation = {
-    NE: data.species.filter((location) => location.location === 'NE')
-      .map((animal) => animal.name),
-    NW: data.species.filter((location) => location.location === 'NW')
-      .map((animal) => animal.name),
-    SE: data.species.filter((location) => location.location === 'SE')
-      .map((animal) => animal.name),
-    SW: data.species.filter((location) => location.location === 'SW')
-      .map((animal) => animal.name),
-};
-  return mapLocation;
-};
+// const zooLocation = () => {
+//   const mapLocation = {
+//     NE: data.species.filter((location) => location.location === 'NE')
+//       .map((animal) => animal.name),
+//     NW: data.species.filter((location) => location.location === 'NW')
+//       .map((animal) => animal.name),
+//     SE: data.species.filter((location) => location.location === 'SE')
+//       .map((animal) => animal.name),
+//     SW: data.species.filter((location) => location.location === 'SW')
+//       .map((animal) => animal.name),
+// };
+//   return mapLocation;
+// };
 
-function getAnimalMap(options) {
-  if (!options || includeNames === undefined) {
-    return zooLocation();
-  }
-  if (includeNames === true) {
-    const mapLocation = {
-      NE: data.species.filter((location) => location.location === 'NE')
-        .map((animal) => animal.residents),
-      NW: data.species.filter((location) => location.location === 'NW')
-        .map((animal) => animal.residents),
-      SE: data.species.filter((location) => location.location === 'SE')
-        .map((animal) => animal.residents),
-      SW: data.species.filter((location) => location.location === 'SW')
-        .map((animal) => animal.residents),
-    }
-  }
-}
+function getAnimalMap(options) {}
+//   if (!options || includeNames === undefined) {
+//     return zooLocation();
+//   }
+//   if (includeNames === true) {
+//     const mapLocation = {
+//       NE: data.species.filter((location) => location.location === 'NE')
+//         .map((animal) => animal.residents),
+//       NW: data.species.filter((location) => location.location === 'NW')
+//         .map((animal) => animal.residents),
+//       SE: data.species.filter((location) => location.location === 'SE')
+//         .map((animal) => animal.residents),
+//       SW: data.species.filter((location) => location.location === 'SW')
+//         .map((animal) => animal.residents),
+//     }
+//   }
+// }
 
 module.exports = getAnimalMap;
